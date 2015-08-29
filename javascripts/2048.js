8f0b65ebd2eeb6e05476675a2b1b4d1006afbc36
jnf <jeremy.flores@gmail.com>
2015-08-29 10:28:46 -0700

    Get a tile moving around.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
   console.log('ready!');
+  $('body').keydown(function(event){
+    var arrow_keys = [37, 38, 39, 40];
+    if(arrow_keys.indexOf(event.which) > -1) {
+      var tile = $('.tile');
+      moveTile(tile, event.which);
+      event.preventDefault();
+    }
+  })
 })
+
+function moveTile(tile, direction) {
+  var new_tile_value = tile.attr("data-val") * 2;
+  tile.attr("data-val", new_tile_value);
+  tile.text(new_tile_value);
+
+  switch(direction) {
+    case 38: //up
+      tile.attr("data-row","r0");
+      break;
+    case 40: //down
+      tile.attr("data-row","r3");
+      break;
+    case 37: //left
+      tile.attr("data-col","c0");
+      break;
+    case 39: //right
+      tile.attr("data-col","c3");
+      break;
+  }
+}
