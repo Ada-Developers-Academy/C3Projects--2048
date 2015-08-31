3589c0066281e09e8956f9371c5d248b60feaac6
MisShellyMac <michellemccarthy@hotmail.com>
2015-08-31 16:00:15 -0700

    wip on random 2nd tile on board generate

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -17,16 +17,33 @@ function generateRandomBoard(){
   var tilesAdded = 0;
   var tilesNum = 2;
   while (tilesAdded < tilesNum){
+
     // Generate random tile position and value
     var randomRow = Math.floor(Math.random() * 4 - 0);
     var randomCol = Math.floor(Math.random() * 4 - 0);
     var newValArray = [2,2,2,2,2,2,2,2,4,4];
     var randomValue = newValArray[Math.floor(Math.random() * newValArray.length)];
     var newTileTemplate = $("<div class='tile' data-row='', data-col='' data-val=''></div>");
+
     // Build new tile
     newTileTemplate.attr("data-row", 'r'+ randomRow);
     newTileTemplate.attr("data-col", 'c'+ randomCol);
     newTileTemplate.attr("data-val", randomValue);
+    newTileTemplate.text(randomValue)
+    // if on the sencond iteration of loops
+    // needs to check for an existing placement of 1st tile
+    // if tilesAdded===1 then check for existing
+    // if no then add
+    // if yes then choose new spot
+    // save 1st tile to variable
+    // do not allow append until new value is different than 1st
+
+    if (tilesAdded === 1){
+      var firstTile = $(".tile");
+        while (newTileTemplate === firstTile){
+
+        }
+    }
 
     // Insert new tile
     $("#gameboard").append(newTileTemplate);
