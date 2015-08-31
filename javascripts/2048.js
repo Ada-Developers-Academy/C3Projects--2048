8edf015f267c93ea8ab3e00591949baa6f912260
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-31 16:13:52 -0700

    Added a board.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
-  console.log('ready!');
+  var board = []
+
+  function begin() {
+    for (i = 0; i < 4; i++) {
+      board[i] = new Array(4);
+    }
+    console.log('ready!');
+  }
+
+  begin();
+
+  // {key: starting position}
+  // {37: c0, 38: r0, 39: c3, 40: r3}
+  // left = 37
+  // up = 38
+  // right = 39
+  // down = 40
+
   $('body').keydown(function(event){
     var arrow_keys = [37, 38, 39, 40];
     if(arrow_keys.indexOf(event.which) > -1) {
       var tile = $('.tile');
+      console.log(tile);
+      console.log(tile.length);
+      console.log(tile[1]);
       moveTile(tile, event.which);
       event.preventDefault();
     }
