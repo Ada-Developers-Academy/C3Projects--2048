abd4d713a64f6cc55dbf20b33e0d9ad3549f67a1
Loraine <loraine@lorainekv.com>
2015-08-31 16:15:05 -0700

    Refactored so that Create Random Tile is a separate function.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -13,23 +13,29 @@ $(document).ready(function() {
   })
 })
 
+function generateRandomTile() {
+  // Generate random tile position and value
+  var randomRow = Math.floor(Math.random() * 4 - 0);
+  var randomCol = Math.floor(Math.random() * 4 - 0);
+  var newValArray = [2,2,2,2,2,2,2,2,4,4];
+  var randomValue = newValArray[Math.floor(Math.random() * newValArray.length)];
+  var newTileTemplate = $("<div class='tile' data-row='', data-col='' data-val=''></div>");
+
+  // Build new tile
+  newTileTemplate.attr("data-row", 'r'+ randomRow);
+  newTileTemplate.attr("data-col", 'c'+ randomCol);
+  newTileTemplate.attr("data-val", randomValue);
+  newTileTemplate.text(randomValue);
+  return newTileTemplate;
+}
+
 function generateRandomBoard(){
   var tilesAdded = 0;
   var tilesNum = 2;
+
   while (tilesAdded < tilesNum){
 
-    // Generate random tile position and value
-    var randomRow = Math.floor(Math.random() * 4 - 0);
-    var randomCol = Math.floor(Math.random() * 4 - 0);
-    var newValArray = [2,2,2,2,2,2,2,2,4,4];
-    var randomValue = newValArray[Math.floor(Math.random() * newValArray.length)];
-    var newTileTemplate = $("<div class='tile' data-row='', data-col='' data-val=''></div>");
-
-    // Build new tile
-    newTileTemplate.attr("data-row", 'r'+ randomRow);
-    newTileTemplate.attr("data-col", 'c'+ randomCol);
-    newTileTemplate.attr("data-val", randomValue);
-    newTileTemplate.text(randomValue)
+    var newTile = generateRandomTile();
     // if on the sencond iteration of loops
     // needs to check for an existing placement of 1st tile
     // if tilesAdded===1 then check for existing
@@ -38,15 +44,19 @@ function generateRandomBoard(){
     // save 1st tile to variable
     // do not allow append until new value is different than 1st
 
-    if (tilesAdded === 1){
-      var firstTile = $(".tile");
-        while (newTileTemplate === firstTile){
-
-        }
-    }
+    // If there's already a tile
+    // if (tilesAdded === 1){
+    //   // Grab existing tile
+    //   var firstTile = $(".tile");
+    //   // Check if new tile and existing tile are the same
+    //   // While they are the same
+    //   while (newTileTemplate === firstTile){
+    //     generateRandomTile();
+    //   }
+    // }
 
     // Insert new tile
-    $("#gameboard").append(newTileTemplate);
+    $("#gameboard").append(newTile);
 
     tilesAdded++;
   }
