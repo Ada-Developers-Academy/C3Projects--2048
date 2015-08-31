6f90b8ddb4f64880ae25bf981986790dea4fc2ef
Elsa <elsa.moluf@gmail.com>
2015-08-31 16:15:46 -0700

    Saving Kari's work that was committed to class master branch.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -2,25 +2,65 @@ $(document).ready(function() {
   console.log('ready!');
   $('body').keydown(function(event){
     var arrow_keys = [37, 38, 39, 40];
-    if(arrow_keys.indexOf(event.which) > -1) {
-      var tile = $('.tile');
-      moveTile(tile, event.which);
-      event.preventDefault();
+
+    if(arrow_keys.indexOf(event.which) > -1) { // meaning the key that was pressed is in the array
+      var tile = $('.tile');                   // all of the tiles
+      moveTile(tile, event.which);             // move all of the tiles in the arrow direction
+      event.preventDefault();                  // prevent default arrow key browser movement
     }
-  })
-})
+  });
+});
 
 function moveTile(tile, direction) {
-  var new_tile_value = tile.attr("data-val") * 2;
-  tile.attr("data-val", new_tile_value);
-  tile.text(new_tile_value);
+  // var new_tile_value = tile.attr("data-val") * 2;
+  // tile.attr("data-val", new_tile_value);
+  // tile.text(new_tile_value);
 
   switch(direction) {
     case 38: //up
-      tile.attr("data-row","r0");
+      var tiles = $('.tile');
+
+      for (i = 0; i < tiles.length; i ++) {
+        var active_tile = tiles[i];
+
+        var data_row_num = parseInt(active_tile.getAttribute('data-row')[1]);
+        var data_col_num = parseInt(active_tile.getAttribute('data-col')[1]);
+
+        for(j = data_row_num ; j >= 0; j--) {
+          var next_row_num = j - 1;
+          var next_tile = $('.tile[data-row="r' + next_row_num + '"][data-col="c' + data_col_num + '"]');
+
+          if (next_tile.length === 0 && next_row_num >= 0 && next_row_num <= 3) { // so the next tile doesn't exist
+            $(active_tile).attr('data-row', "r" + next_row_num); // move the tile to that position
+          }
+        }
+      }
       break;
+
     case 40: //down
-      tile.attr("data-row","r3");
+
+      // for each row
+      for (i = 3; i >= 0; i--) {
+        var tiles = $('.tile[data-row="r' + i);
+
+        for (i = 0; i < tiles.length; i ++) {
+          var active_tile = tiles[i];
+
+          var data_row_num = parseInt(active_tile.getAttribute('data-row')[1]);
+          var data_col_num = parseInt(active_tile.getAttribute('data-col')[1]);
+
+          for(j = data_row_num ; j < 4; j++) {
+            var next_row_num = j + 1;
+            var next_tile = $('.tile[data-row="r' + next_row_num + '"][data-col="c' + data_col_num + '"]');
+
+            if (next_tile.length === 0 && next_row_num >= 0 && next_row_num <= 3) { // so the next tile doesn't exist
+              $(active_tile).attr('data-row', "r" + next_row_num); // move the tile to that position
+            }
+          }
+        }
+      }
+
+      // tile.attr("data-row","r3");
       break;
     case 37: //left
       tile.attr("data-col","c0");
