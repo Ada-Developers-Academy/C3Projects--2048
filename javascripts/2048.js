8051d21b9f7b28c39804249d2e416f0eb91abd2d
Victoria <vikshab2010@hotmail.com>
2015-08-31 15:25:59 -0700

    Added tilePlacement and position functions.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
       event.preventDefault();
     }
   })
+ tilePlacement(["r3", "c2"]);
 })
 
 function moveTile(tile, direction) {
@@ -30,3 +31,26 @@ function moveTile(tile, direction) {
       break;
   }
 }
+
+function position(){
+  var rowCoordinates = ["r0", "r1", "r2", "r3"];
+  var columnCoordinates = ["c0", "c1", "c2", "c3"];
+
+  var randomRow = Math.floor(Math.random() * (rowCoordinates.length));
+  var randomColumn = Math.floor(Math.random() * (columnCoordinates.length));
+
+  var position = [rowCoordinates[randomRow], columnCoordinates[randomColumn]];
+
+  return position;
+}
+
+function tilePlacement(position) {
+    var tileDiv =  $("<div class='tile'></div>");
+    tileDiv.text(2);
+    tileDiv.attr("data-row", position[0]);
+    tileDiv.attr("data-col", position[1]);
+    tileDiv.attr("data-val",  '2');
+
+    $("#gameboard").append(tileDiv);
+    console.log(tileDiv);
+  }
