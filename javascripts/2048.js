a24ec5f592ec1af0b1a5b7f145d4f416da258d28
Carly Jugler <carjug@gmail.com>
2015-08-31 15:03:54 -0700

    Adds 2D array builder and random assignment of 2 to two positions in 2D array

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   console.log('ready!');
+  initializeBoard();
   $('body').keydown(function(event){
     var arrow_keys = [37, 38, 39, 40];
     if(arrow_keys.indexOf(event.which) > -1) {
@@ -9,6 +10,43 @@ $(document).ready(function() {
     }
   })
 })
+
+var board     = [];
+var boardSize = 4;
+
+// sets up 2D array
+function initializeBoard() {
+  for(var i = 0; i < boardSize; i++) {
+    innerBoard = [];
+    board.push(innerBoard);
+
+    for(var j = 0; j < boardSize; j++) {
+      board[i][j] = 0;
+      }
+    }
+    assignRandoTiles();
+    assignRandoTiles();
+    console.log(board.toString());
+  }
+
+  // sets the position of starter tiles in 2D array -- currently only setting
+  // value to 2
+  function assignRandoTiles() {
+    var y = Math.floor(Math.random() * (boardSize));
+    var x = Math.floor(Math.random() * (boardSize));
+    if(board[y][x] == 0) {
+      board[y][x] = 2;
+    }
+    else {
+      assignRandoTiles();
+      // this will run endlessly when the board is full -- write a conditional for that
+    }
+  }
+
+
+
+
+
 
 function moveTile(tile, direction) {
   var new_tile_value = tile.attr("data-val") * 2;
