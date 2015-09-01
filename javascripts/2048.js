3e87c400ff6e0cf8158a706941b4bbd1ddbdee90
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-31 17:23:27 -0700

    updated with new row/column numbering

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -2,31 +2,43 @@ $(document).ready(function() {
   console.log('ready!');
   $('body').keydown(function(event){
     var arrow_keys = [37, 38, 39, 40];
-    if(arrow_keys.indexOf(event.which) > -1) {
-      var tile = $('.tile');
-      moveTile(tile, event.which);
+    var direction = event.which;
+    if(arrow_keys.indexOf(direction) > -1) {
+      moveTiles(direction);
       event.preventDefault();
     }
   })
 })
 
+function moveTiles(direction) {
+  var tiles = $(".tile");
+  for (i = 0; i < tiles.length; i++) {
+    moveTile(tiles[i], direction);
+  }
+}
+
 function moveTile(tile, direction) {
-  var new_tile_value = tile.attr("data-val") * 2;
-  tile.attr("data-val", new_tile_value);
-  tile.text(new_tile_value);
+  var farthestEdgeType = "";
+  var farthestEdgeValue = "";
 
   switch(direction) {
-    case 38: //up
-      tile.attr("data-row","r0");
+    case 38: // up
+      farthestEdgeType = "data-row";
+      farthestEdgeValue = "1";
       break;
-    case 40: //down
-      tile.attr("data-row","r3");
+    case 40: // down
+      farthestEdgeType = "data-row";
+      farthestEdgeValue = "4";
       break;
-    case 37: //left
-      tile.attr("data-col","c0");
+    case 37: // left
+      farthestEdgeType = "data-col";
+      farthestEdgeValue = "1";
       break;
-    case 39: //right
-      tile.attr("data-col","c3");
+    case 39: // right
+      farthestEdgeType = "data-col";
+      farthestEdgeValue = "4";
       break;
   }
+
+  tile.setAttribute(farthestEdgeType, farthestEdgeValue);
 }
