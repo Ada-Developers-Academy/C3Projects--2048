2666d90035d20c44336508d1f59005ef7b7b13b3
Sally Moore <sallyamoore@gmail.com>
2015-08-31 17:18:09 -0700

    Added functionality to add first two tiles in random locations and with random value of either 2 or 4.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,4 +1,12 @@
+var rows = ['r0', 'r1', 'r2', 'r3'];
+var cols = ['c0', 'c1', 'c2', 'c3'];
+var startValues = [ 2, 4 ];
+
 $(document).ready(function() {
+
+
+  initializeGame();
+
   console.log('ready!');
   $('body').keydown(function(event){
     var arrow_keys = [37, 38, 39, 40];
@@ -7,8 +15,32 @@ $(document).ready(function() {
       moveTile(tile, event.which);
       event.preventDefault();
     }
-  })
-})
+  });
+});
+
+function initializeGame() {
+  // randomly pick two positions and start values
+  generateTile(startValues);
+  generateTile(startValues);
+}
+
+function generateTile(array) {
+  var tile = $('<div class="tile" data-row="", data-col="" data-val="">');
+  var randomRow = Math.floor(Math.random() * (rows.length));
+  var randomCol = Math.floor(Math.random() * (cols.length));
+  var randomValue = Math.floor(Math.random() * (array.length));
+
+  tile.attr('data-row', rows[randomRow]);
+  tile.attr('data-col', cols[randomCol]);
+  tile.attr('data-val', array[randomValue]);
+  tile.text(array[randomValue]);
+
+  if ($('[data-row=' + rows[randomRow] + '][data-col=' + cols[randomCol] + ']').length === 0) {
+    $('#gameboard').append(tile);
+  } else {
+    generateTile(array);
+  }
+}
 
 function moveTile(tile, direction) {
   var new_tile_value = tile.attr("data-val") * 2;
