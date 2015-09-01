e4fc9058a06666c8fb6600517f43afd6ffb8c1e9
Jeri <drvonnjerryxlii@gmail.com>
2015-09-01 11:04:44 -0700

    added method to reorient the board

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -32,38 +32,60 @@ function moveTile(tile, direction) {
 }
 
 
-// board = new Board([]);
 var Board = function(boardArray) { // board constructor
   this.board = boardArray;
-}
+};
 
-Board.protoype.update(direction) {
+board = new Board([
+  [2,     2,   0,   0], // [2,   4,  16, 512],
+  [4,     8,   4,   2], // [2,   8,  32, 256],
+  [16,   32, 128,  64], // [0,   4, 128, 128],
+  [512, 256, 128,  32] //  [0,   2,  64,  32]
+]);
+
+console.log(board.board);
+
+Board.prototype.move = function(direction) {
   // this is the movement function
-  // var reorientedBoard = this.split();
-  // this.condense();
-}
+  var reorientedBoard = this.split(direction);
+  console.log(reorientedBoard);
+  // var condensedBoard = reorientedBoard.forEach(this.condense); // not 100% certain about this syntax
+};
 
 // board.split()
-Board.prototype.split() {
+Board.prototype.reorient = function(direction) {
   // splits the board into arrays based on direction
-}
+  // this should have access to the direction from board.update();
+  var method;
+  if (direction == "left" || direction == "right")
+    method = "leftReorient";
+  else // "up" || "down"
+    method = "downReorient";
+
+  return this[method].call(this); // execute the method in the current context
+};
+
+Board.prototype.leftReorient = function() {
+  return this.board; // or do we want to modify the board in place?
+};
+
+Board.prototype.downReorient = function() {
+  var reorientedBoard = [];
+
+  for (var oldCol = 0; oldCol < 4; oldCol++) {
+    var newRow = [];
+
+    for (var oldRow = 0; oldRow < 4; oldRow++) {
+      newRow.push(this.board[oldRow][oldCol]);
+    };
+
+    reorientedBoard.push(newRow);
+  };
+
+  return reorientedBoard;
+};
 
 // board.condense()
-Board.prototype.condense() {
+Board.prototype.condense = function() {
   // condensing the array (removing empty spaces / 0s)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
