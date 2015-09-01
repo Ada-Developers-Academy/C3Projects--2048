b9ac56531ea9705985ea317f70d8a7a0feaa897e
amirahaile <amira.dhaile@gmail.com>
2015-09-01 13:22:52 -0700

    Refactored the function that determines direction into the function that orders the tiles.

--- javascripts/tile_collision.js
+++ javascripts/tile_collision.js
@@ -11,32 +11,14 @@ function tileInventory() {
   return takenSpaces;
 }
 
-function orderDirection(direction) {
-  switch(direction) {
-    case 38: // up
-    // bottom, middle, top
-
-      break;
-    case 40: // down
-    // top, middle, bottom
-      break;
-    case 37: // left
-    // right, middle, left
-      break;
-    case 39: // right
-    // left, middle, right
-      break;
-  }
-}
-
-function orderTiles(start, tiles) {
+function orderTiles(keystroke, tiles) {
   // the reaction of tiles colliding happens in the order opposite
   // of the keystroke (e.g. up key evalautes the bottom row first)
   var direction = function() {
-    if (start === "r3") { return ["r3", "r2", "r1", "r0"]; } // up
-    if (start === "r0") { return ["r0", "r1", "r2", "r3"]; } // down
-    if (start === "c3") { return ["c3", "c2", "c1", "c0"]; } // left
-    if (start === "c0") { return ["c0", "c1", "c2", "c3"]; } // right
+    if (keystroke === 38) { return ["r3", "r2", "r1", "r0"]; } // up
+    if (keystroke === 40) { return ["r0", "r1", "r2", "r3"]; } // down
+    if (keystroke === 37) { return ["c3", "c2", "c1", "c0"]; } // left
+    if (keystroke === 39) { return ["c0", "c1", "c2", "c3"]; } // right
   };
 
   // determines which index to use to organize the tiles
