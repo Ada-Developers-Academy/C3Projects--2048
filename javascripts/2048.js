c45be6d3ec75baab0155ffe74c20fe5957939d92
Lindsey Reno <lindseyannereno@gmail.com>
2015-09-02 10:07:52 -0700

    Added comments, completed addTile function

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -32,30 +32,27 @@ function moveTile(tile, direction) {
 }
 
 function game_Lost(){
-  var gameLost = false
+  var gameLost = false;
 
-  if $("#gameboard .tile").length == 16 
-    var gameLost = true 
-  // && else if no collisions possible  
-    var gameLost = true
-  end  
-      
- return gameLost 
+  if (($("#gameboard .tile").length == 16) ) {  // && (no collisions possible)
+    var gameLost = true;
+  }
+
+ return gameLost;
 }
 
 function game_Won(){
-  var gameWon = false
-  // pseudo : loop through all the tiles, check value.
-  // if tile with value "2048", gameWon true
+  var gameWon = false;
 
   var gameBoard = $("#gameboard .tile")
-  for (var i = 0; i < gameBoard; i++){
-     if gameBoard[i].attr("data-val") >= 2048
-        gameWon = true  
+  for (var i = 0; i < gameBoard; i++){  // loop through all the tiles, check value
+     if (gameBoard[i].attr("data-val") >= 2048) {    // if tile with value "2048", gameWon true
+
+        gameWon = true;
       }
+    }
 
-return gameWon
-
+return gameWon;
 }
 
 function freeSpot() {
@@ -63,50 +60,59 @@ function freeSpot() {
  for (var row = 0; row < 4; row++) {
    for (var col = 0; col < 4; col++) {
      var reference = ".tile[data-row=r" + row + "][data-col=c" + col + "]";
-     // => .tile[data-row=r0][data-col=c0]
+     // => .tile[data-row=r0][data-col=c0] => occupied
      var unoccupied = $(reference).length == 0;
      if(unoccupied)
        freeSpots.push({ row: row, col: col });
    }
  }
 
- return freeSpots[Math.floor( Math.random() * freeSpots.length )];
+ return freeSpots[Math.floor( Math.random() * freeSpots.length )]; // => returns hash
 }
 
-function add_tile(){
-  var newTileHere = pick freeSpot()
-  var newTileHere = 
-  var tile = make tile(here)
-  insert_tile()
+function addTile(){
+  var newSpot = freeSpot();
+  var newDiv = document.createElement("div");
+  var newClass = document.createAttribute("class");
+  newClass.value = "tile";
+  newDiv.setAttributeNode(newClass);
+  var dataRow = document.createAttribute("data-row");
+  dataRow.value = "r" + newSpot["row"];
+  newDiv.setAttributeNode(dataRow);
+  var dataCol = document.createAttribute("data-col");
+  dataCol.value = "c" + newSpot["col"];
+  newDiv.setAttributeNode(dataCol);
+  var dataVal = document.createAttribute("data-val");
+  dataVal.value = "2";
+  newDiv.setAttributeNode(dataVal);
+  newDiv.textContent = "2";
+
+  $("#gameboard").append(newDiv); // adds tile to gameboard
 
 }
 
-pseudo:
-when game starts: two random tiles populate board
-is game over?
-  true or false
-  (like battleship -> start in false)
-  game over: 
-    lose:any empty spaces && no possible collisions?  
-    win:tile value "2048" 
-click event (arrow_keys)
-  movetile function happens
-  collisions result in new tile with increased 
-  face value
-    if tile adjacent in direction of arrow is identical
-    in face value to the one next to it, the farthest tile is replaced with new tile 
-     -> double face value
-    any additional tiles behind that merge shift one position in same direction  
-  new value from collision of tile in farthest most position
-score updated to reflect any/all collisions (+=)
-new random tile enters the board
-  check for empty spaces
-  randomly select value of 2 or 4 to random tile
-  place random tile in random empty spot
-start over - is game over? 
 
 
-
-
-
-
+// pseudo:
+// when game starts: two random tiles populate board
+// is game over?
+//   true or false
+//   (like battleship -> start in false)
+//   game over:
+//     lose:any empty spaces && no possible collisions?
+//     win:tile value "2048"
+// click event (arrow_keys)
+//   movetile function happens
+//   collisions result in new tile with increased
+//   face value
+//     if tile adjacent in direction of arrow is identical
+//     in face value to the one next to it, the farthest tile is replaced with new tile
+//      -> double face value
+//     any additional tiles behind that merge shift one position in same direction
+//   new value from collision of tile in farthest most position
+// score updated to reflect any/all collisions (+=)
+// new random tile enters the board
+//   check for empty spaces
+//   randomly select value of 2 or 4 to random tile
+//   place random tile in random empty spot
+// start over - is game over?
