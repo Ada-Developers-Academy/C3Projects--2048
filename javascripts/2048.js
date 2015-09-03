662d8eac8d8d2a15fbaefef150727d7f341a5a3a
Shanna Chau <shannachau@gmail.com>
2015-09-03 15:55:21 -0700

    Changing the nested of the outermost if else if block the Array equals function. Was causing bug where it was incorrectly evaluating similar arrays as not equal

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -11,19 +11,20 @@ Array.prototype.equals = function (array) {
     return false;
   }
 
-  for (var i = 0, l=this.length; i < l; i++) {
+  for (var i = 0; i < this.length; i++) {
     // Check if we have nested arrays
     if (this[i] instanceof Array && array[i] instanceof Array) {
       // recurse into the nested arrays
       if (!this[i].equals(array[i])) {
         return false;
-      } else if (this[i] != array[i]) {
-        // Warning - two different object instances will never be equal: {x:20} != {x:20}
-        return false;
       }
+    } else if (this[i] != array[i]) {
+        // Warning - two different object instances will never be equal: {x:20} != {x:20}
+      return false;
     }
   }
   return true;
+
 }
 
 $(document).ready(function() {
@@ -52,6 +53,7 @@ function makeTurn(direction) {
   var startingGameboard = gameboardSnapshot($(".tile"));
 
   function gameboardSnapshot(tiles) {
+    // NEW WAY
     var gameboard = [];
     for (var i = 0; i < tiles.length; i++) {
       var dataRow = tiles[i].getAttribute("data-row");
